33b7a9a9e32e06bba39acfd0558e05ef131193cd
Jesse Chen <0t2jesse@gmail.com>
2017-06-07 10:05:38 +0800

    initial commit

---
+++ src/main/java/me/_0t2/SubmoduleDemoApplication.java
@@ -0,0 +1,12 @@
+package me._0t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmoduleDemoApplication.class, args);
+	}
+}
